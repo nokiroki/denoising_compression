cfa6f520cf21f9431840dd987f4d8ec361de354e
nokiroki <artem.krotov42@gmail.com>
2022-03-22 11:29:28 +0300

    comm

--- main.py
+++ main.py
@@ -130,10 +130,8 @@ def main_func(plot=False):
         psrn_gt = compare_psnr(img_np, out.detach().cpu().numpy()[0])
         psrn_gt_sm = compare_psnr(img_np, out_avg.detach().cpu().numpy()[0])
 
-        print(img_noisy_np.shape)
-        print(out.detach().cpu().numpy()[0].shape)
-        ssim_noisy = compare_ssim(img_noisy_np, out.detach().cpu().numpy()[0], multichannel=True)
-        ssim_gt = compare_ssim(img_np, out.detach().cpu().numpy()[0], multichannel=True)
+        ssim_noisy = compare_ssim(img_noisy_np.T, out.detach().cpu().numpy()[0].T, multichannel=True)
+        ssim_gt = compare_ssim(img_np.T, out.detach().cpu().numpy()[0].T, multichannel=True)
 
         # Note that we do not have GT for the "snail" example
         # So 'PSRN_gt', 'PSNR_gt_sm' make no sense
