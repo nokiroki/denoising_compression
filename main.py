8bd0c8f34f6efe49c8b9152bec32562a2a1f25c3
EvgeniaKomleva <36705500+EvgeniaKomleva@users.noreply.github.com>
2022-03-23 09:17:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from skimage.metrics import peak_signal_noise_ratio as compare_psnr,\
     structural_similarity as compare_ssim
 
 
-def main_func(plot=False, num_iters=1500):
+def main_func(plot=False, num_iters=10):
 
     torch.backends.cudnn.enabled = True
     torch.backends.cudnn.benchmark =True
